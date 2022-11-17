a254fbf3eb4aa764cf6c64a3f70967adea706424
Vova Yankers <vovarojko@gmail.com>
2022-11-17 12:31:58 +0200

    half created calc

--- Раздел 5. Продвинутый JavaSript/97. Создаем калькулятор на сайте, часть 1/Food/js/script.js
+++ Раздел 5. Продвинутый JavaSript/97. Создаем калькулятор на сайте, часть 1/Food/js/script.js
@@ -472,19 +472,80 @@ window.addEventListener('DOMContentLoaded', function() {
     // Calc
 
     const result = document.querySelector('.calculating__result span');
-    let sex, height, weight, age, ratio;
+    let sex = 'female', 
+    height, weight, age, 
+    ratio = 1.375;
 
     function calcTotal() {
         if (!sex || !height || !weight || !age ||  !ratio) {
             result.textContent = "____";
             return; // после return уже ничего работать не будет
             // елси у нас всё в порядке, то
-        } else { 
+        } 
+
             if (sex === 'female') {
-                result.textContent = () * ratio;
+                result.textContent = ((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio).toFixed(2);
+            } else {
+                // male
+                result.textContent = ((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio).toFixed(2);
             }
         }
-    }
+    
+    calcTotal();
 
-    console.log(3 || 2);
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    // именно клик на определённое значение (активность) - вытаскиваем её
+                    // если кликием в блок GENDER - то значение не будет срабатывать так как мы указываем уник атрибут
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+                console.log(ratio, sex);
+
+                // работаем тперь с классави активности
+                // Перебираем через родительский полученный класс
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+
+                e.target.classList.add(activeClass);
+
+                calcTotal();
+        });
+        });
+      
+    }
+                        // Parent           classActive
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+// Делаем функцию для input: height, weight, age
+    function getDinamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () =>{
+            switch(input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            calcTotal();
+        });
+      
+      
+    }
+    getDinamicInformation('#height');
+    getDinamicInformation('#weight');
+    getDinamicInformation('#age');
+    
 });
