30300dccedb06a45232abaf85e1f851ea829e34c
Vova Yankers <vovarojko@gmail.com>
2023-02-14 11:38:25 +0200

    Works with chiildNode and childElements

--- Раздел 2, Основы JavaScript/46. Навигация по DOM - элементам, data-атрибуты, преимущество forof/script.js
+++ Раздел 2, Основы JavaScript/46. Навигация по DOM - элементам, data-атрибуты, преимущество forof/script.js
@@ -1,40 +1,59 @@
-// const body = document.body;
-// console.log(body);
+// const body = document.body
+// console.log(body)
 
 // console.log(document.body);
-// console.log(document.documentElement);
-//console.log(document.body.childNodes); // все Node и узлы этого родителя
+// console.log(document.head)
+// console.log(document.documentElement) //HTML
+// console.log(document.body.childNodes) // все Node и узлы этого родителя
 
-// console.log(document.body.firstChild);
-// console.log(document.body.firstElementChild);
-
+// console.log(document.body.firstChild)
+// console.log(document.body.lastChild)
 
 // console.log(document.body.lastChild);
 
-// console.log(document.querySelector('#current').parentNode);
+// const buttonOne = document.querySelector('button')
+// const getButton = (e) => {
+//   console.log(e.currentTarget)
+//   console.log(document.querySelector('#current').parentNode.parentNode)
+// }
+// buttonOne.addEventListener('click', getButton)
+// console.log(document.querySelector('#current').parentNode)
 
 // console.log(document.querySelector('#current').parentNode.parentNode);
 // console.log(document.querySelector('#current').parentElement);
 
-// console.log(document.querySelector('[data-current="3"]'));
-// console.log(document.querySelector('[data-current="3"]').nextSibling);
-// console.log(document.querySelector('[data-current="3"]').previousSibling);
+// console.log(document.querySelector('[data-current="3"]'))
+// console.log(document.querySelector('[data-current="3"]').nextSibling)
+// console.log(document.querySelector('[data-current="3"]').previousSibling)
 
 // console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 // console.log(document.querySelector('[data-current="3"]').previousElementSibling);
+// const buttonOne = document.querySelector('button')
+// let li_5 = document.querySelector('[data-current="5"]')
 
-// 
-// for (let node of document.body.childNodes) {
-//     if( node.nodeName == '#text') {
-//         continue;
-        
-//     }
-//    const allElement = node;
-//    console.log(allElement);
+// const changeLi = () => {
+//   console.log(li_5)
+//   li_5.style.color = 'orange'
+//   li_5.style.fontSize
+//     ? (li_5.style.fontSize = '30px')
+//     : (li_5.style.fontSize = '10px')
 // }
 
-const btns = document.getElementsByTagName('button');
-console.dir(btns);
+// buttonOne.addEventListener('click', changeLi)
 
-const buttons = document.querySelectorAll('button');
-console.dir(buttons);
+// console.log(document.querySelector('#current').parentNode)
+
+console.log(document.body.childNodes)
+for (let node of document.body.childNodes) {
+  if (node.nodeName == '#text' || node.nodeName == '#comment') {
+    continue
+  }
+  //   let arrElement = [...node]
+  //   console.log(arrElement)
+}
+
+// const btns = document.getElementsByTagName('button')
+// console.dir(btns)
+
+// const buttons = document.querySelectorAll('button')
+// console.dir(buttons)
