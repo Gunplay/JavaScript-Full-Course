0be1c102a151702511a376e90d2dc16ab3ea31e6
Vova Yankers <vovarojko@gmail.com>
2023-04-19 13:25:28 +0300

    Project exam add Characters

---
+++ Раздел 9 React + Redux/197. Правило названия action и домашнее задание (мини-экзамен)/hero_admin_panel_template/src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react'
+import { useHttp } from '../../hooks/http.hook'
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров
+
+const HeroesAddForm = () => {
+  const { request } = useHttp()
+
+  const [textName, setTextName] = useState('')
+  const [textArea, settextArea] = useState('')
+  const [filtres, setFiltres] = useState([])
+  console.log('filtres', filtres)
+  useEffect(() => {
+    async function fetchData() {
+      const response = await request('http://localhost:3001/filters')
+
+      setFiltres(response)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <form className="border p-4 shadow-lg rounded">
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label fs-4">
+          Имя нового героя
+        </label>
+        <input
+          required
+          value={textName}
+          onChange={(e) => setTextName(e.target.value)}
+          type="text"
+          name="name"
+          className="form-control"
+          id="name"
+          placeholder="Как меня зовут?"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="text" className="form-label fs-4">
+          Описание
+        </label>
+        <textarea
+          required
+          value={textArea}
+          onChange={(e) => settextArea(e.target.value)}
+          name="text"
+          className="form-control"
+          id="text"
+          placeholder="Что я умею?"
+          style={{ height: '130px' }}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="element" className="form-label">
+          Выбрать элемент героя
+        </label>
+        <select required className="form-select" id="element" name="element">
+          {filtres.map((filter, index) => (
+            <option value={filter} key={index}>
+              {filter}
+            </option>
+          ))}
+          {/* <option>Я владею элементом...</option>
+          <option value="fire">Огонь</option>
+          <option value="water">Вода</option>
+          <option value="wind">Ветер</option>
+          <option value="earth">Земля</option> */}
+        </select>
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Создать
+      </button>
+    </form>
+  )
+}
+
+export default HeroesAddForm
